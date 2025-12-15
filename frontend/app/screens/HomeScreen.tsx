147869dce8fc52ccf18a45b17b087963ecddc2f3
europanite <europanite@gmail.com>
2025-12-15 16:23:45 +0900

    fix/doc

--- frontend/app/screens/HomeScreen.tsx
+++ frontend/app/screens/HomeScreen.tsx
@@ -1027,7 +1027,6 @@ const HomeScreen: React.FC = () => {
                 marginBottom: 12,
                 color: "#1d4ed8",
                 textDecorationLine: "underline",
-                underlined: true,
               }}
             >
               DET Vocabulary Practice
